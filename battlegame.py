a6a2ad8b66fe1a82129facf5993e1cbea8d045a6
Aaron <Aaron.Slowinski@gmail.com>
2024-03-20 20:49:50 -0700

    battle game wk 1

---
+++ battlegame.py
@@ -0,0 +1,28 @@
+wizard = "Wizard"
+elf = "Elf"
+human = "Human"
+orc = "Orc"
+exit = "exit"
+
+
+
+wizard_hp = 70
+elf_hp = 100
+human_hp = 150
+orc_hp = 200
+dragon_hp = 300
+
+wizard_damage = 150
+elf_damage = 100
+human_damage = 20
+orc_damage = 30
+dragon_damage = 50
+
+print("Choose your character:")
+print("1. Wizard")
+print("2. Elf")
+print("3. Human")
+print("4. Orc")
+print("5. Exit")
+
+character_choice = input("Choose your character: ")
