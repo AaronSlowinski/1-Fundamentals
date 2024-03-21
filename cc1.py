dec9e143637ce85e803a4d393eddcc3c1ad11136
Aaron <Aaron.Slowinski@gmail.com>
2024-03-20 18:46:17 -0700

    python practice

---
+++ cc1.py
@@ -0,0 +1,21 @@
+# Turn this line into a comment #
+
+cities = ["Seattle", "Tacoma", "Bellevue"] # updated the list of cities to have double quotes instead of single quotes #
+
+a = 1
+print(type(a))
+
+b = 97.5
+print(type(b))
+
+c = "NuCamp Python"
+print(type(c))
+
+d = a+b
+print(d)
+
+
+
+
+
+
