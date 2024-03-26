588b6c932bafecb61f456454950b2d50d3f43095
Aaron <Aaron.Slowinski@gmail.com>
2024-03-25 21:16:19 -0700

    dice game

--- cc_dicegame.py
+++ cc_dicegame.py
@@ -1,9 +1,30 @@
 import random
-
 high_score = 0
 
-
 def dice_game():
+    global high_score
+    print("\nWelcome to the Dice Game!\n")
+    print("You will roll two dice and try to get the highest score possible.\n")
+    print("Current high score is: ", high_score , "\n")
 
+    while True:
+        print("1. Roll the dice\n")
+        print("2. Quit the game\n")
+        choice = input("Enter your choice: ")
+
+        if choice == "2":
+            print("Goodbye!")
+            break
+        elif choice == "1":
+            die1 = random.randint(1, 6)
+            die2 = random.randint(1, 6)
+            total = die1 + die2
+            print("\n You rolled a", die1, "and a", die2, "for a total of ", total ,"\n")
+
+            if total > high_score:
+                high_score = total
+                print("\n Congratulations! You have a new high score of ", high_score ,"\n")
+        else:
+            print("Invalid choice. Please try again.")
 
 dice_game()
