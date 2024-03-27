3bfa49a725cd7c33294a2d41eb742502d09c6455
Aaron <Aaron.Slowinski@gmail.com>
2024-03-26 19:45:04 -0700

    dice roll with functions

--- cc_dicegame.py
+++ cc_dicegame.py
@@ -1,11 +1,15 @@
 import random
+
 high_score = 0
+
+def roll_die():
+    return random.randint(1, 6)
 
 def dice_game():
     global high_score
     print("\nWelcome to the Dice Game!\n")
     print("You will roll two dice and try to get the highest score possible.\n")
-    print("Current high score is: ", high_score , "\n")
+    print("Current high score is:", high_score, "\n")
 
     while True:
         print("1. Roll the dice\n")
@@ -16,14 +20,14 @@ def dice_game():
             print("Goodbye!")
             break
         elif choice == "1":
-            die1 = random.randint(1, 6)
-            die2 = random.randint(1, 6)
+            die1 = roll_die()
+            die2 = roll_die()
             total = die1 + die2
-            print("\n You rolled a", die1, "and a", die2, "for a total of ", total ,"\n")
+            print("\nYou rolled a", die1, "and a", die2, "for a total of", total, "\n")
 
             if total > high_score:
                 high_score = total
-                print("\n Congratulations! You have a new high score of ", high_score ,"\n")
+                print("\nCongratulations! You have a new high score of", high_score, "\n")
         else:
             print("Invalid choice. Please try again.")
 
