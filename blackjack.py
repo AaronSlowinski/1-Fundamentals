e05a458fdd10649c5855f196ee8d485782c857fc
Aaron <Aaron.Slowinski@gmail.com>
2024-03-30 19:30:15 -0700

    blackjack game

---
+++ blackjack.py
@@ -0,0 +1,15 @@
+import random, sys
+
+# Constants
+HEARTS = 'Hearts'
+DIAMONDS = 'Diamonds'
+SPADES = 'Spades'
+CLUBS = 'Clubs'
+
+BACKSIDE='backside'
+
+def main():
+    print('Welcome to Blackjack!')
+    print('You will be playing against the computer.')
+    print('You start with $1000. Bet wisely.')
+   
