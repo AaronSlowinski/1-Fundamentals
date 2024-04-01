265345aca16113737bd3ea4665beb1e0c4f9df47
Aaron <Aaron.Slowinski@gmail.com>
2024-03-31 20:50:34 -0700

    black jack suits

--- blackjack.py
+++ blackjack.py
@@ -12,4 +12,12 @@ def main():
     print('Welcome to Blackjack!')
     print('You will be playing against the computer.')
     print('You start with $1000. Bet wisely.')
-   
+    
+while True:
+    suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
+    ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+    deck = []
+    for suit in suits:
+        for rank in ranks:
+            deck.append(f"{rank} of {suit}")
+    random.shuffle(deck)
