e3ecaf4f66db6b5055f35c0e9a3331bc5ad40dd9
Aaron <Aaron.Slowinski@gmail.com>
2024-04-01 19:17:06 -0700

    slicing a list

--- Week3/using_list2.py
+++ Week3/using_list2.py
@@ -5,7 +5,16 @@ for state in states:
     state= state.lower()
     print(state)
 """
-
+"""
 print ("Washington" in states)
 print ("Texas" in states)
 print ("Washington" not in states)
+"""
+
+states2= ["Arizona", "Ohio", "Louisiana"]
+best_states = states + states2
+
+print(best_states)  
+print(best_states[1:3])
+print(best_states[:2])
+print(best_states[4:])
