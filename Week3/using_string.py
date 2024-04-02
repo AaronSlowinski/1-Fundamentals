e71566dc2c32d28a8fcfb3fb1d821e6d8487aca1
Aaron <Aaron.Slowinski@gmail.com>
2024-04-01 19:59:34 -0700

    strings

---
+++ Week3/using_string.py
@@ -0,0 +1,26 @@
+my_string = "alpha"
+
+'''
+multi_line_string = """This is a
+multi-line string"""
+
+print(my_string)
+print(multi_line_string)
+
+
+print(my_string[0])
+print(my_string[3)
+
+
+
+print(my_string[0:3])
+print(my_string[:2])
+
+
+for char in my_string:
+    print(char) # a l p h a
+    
+'''
+
+print ("pha" in my_string) # True
+print ("z" in my_string) # False
