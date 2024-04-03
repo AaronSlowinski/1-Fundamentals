e0d41fe286bb7200a58b828d5bf2f845ad6c4029
Aaron <Aaron.Slowinski@gmail.com>
2024-04-02 20:04:14 -0700

    login option

--- Week3/WORKSHOP3/app.py
+++ Week3/WORKSHOP3/app.py
@@ -6,6 +6,7 @@ Last updated:
 """
 
 from donations_pkg.homepage import show_homepage
+from donations_pkg.user import login
 
 database = {"admin": "password123"}
 donations = []
@@ -17,4 +18,24 @@ while True:
         print("You must be logged in to donate.")
     else:
         print("Logged in as:", authorized_user)
-    break
+    
+    option = input("Please select an option: ")
+    
+    if option == "1":
+        print("Login")
+        username = input("Enter your username: ")
+        password = input("Enter your password: ")
+        authorized_user = login(database, username, password)
+        print("\n")
+    elif option == "2":
+        print("TODO: Write Register Functionality")
+    elif option == "3":
+        print("TODO: Write Donate Functionality")
+    elif option == "4":
+        print("TODO: Write Show Donations Functionality")
+    elif option == "5":
+        print("Goodbye!")
+        break
+    else:
+        print("Invalid option. Please try again.")
+
--- Week3/WORKSHOP3/donations_pkg/user.py
+++ Week3/WORKSHOP3/donations_pkg/user.py
@@ -1,5 +1,11 @@
-def login (database, username, password):
-    for user in database:
-        if user['username'] == username and user['password'] == password:
-            return True
-    return False
+
+def login(database, username, password):
+    if username in database and database[username] == password:
+        print("Welcome,", username)
+        return username
+    elif username in database and database[username] != password:
+        print("Incorrect password")
+        return ""
+    else:
+        print("Username not found")
+        return ""
