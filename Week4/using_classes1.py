41e4d560166a5143ae45600e2f32978c5d9dae77
Aaron <Aaron.Slowinski@gmail.com>
2024-04-08 11:34:50 -0700

    changing hp class attri

--- Week4/using_classes1.py
+++ Week4/using_classes1.py
@@ -1,9 +1,13 @@
 class Player:
-    max_hp = 4000
+    max_hp = 4000 #class attribute shared by all instances of the class
     
 player1 = Player()
 print(player1.max_hp)
 
 player2 = Player()
 print(player2.max_hp)
-    
+
+Player.max_hp = 5000 #changing the class attribute
+print(player1.max_hp)
+print(player2.max_hp)
+   
